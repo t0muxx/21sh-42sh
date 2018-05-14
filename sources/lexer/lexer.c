7d484a92299e820da6232bd947f58e7d2bea87c2
cormarti <cormarti@student.42.fr>
2018-05-14 11:50:28 +0200

    fix conflicts

--- sources/lexer/lexer.c
+++ sources/lexer/lexer.c
@@ -5,19 +5,13 @@
 /*                                                    +:+ +:+         +:+     */
 /*   By: cormarti <marvin@42.fr>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
-<<<<<<< HEAD
 /*   Created: 2018/03/27 23:49:09 by cormarti          #+#    #+#             */
-/*   Updated: 2018/03/31 04:51:41 by cormarti         ###   ########.fr       */
-=======
-/*   Created: 2018/03/22 10:43:25 by tmaraval          #+#    #+#             */
-/*   Updated: 2018/03/27 17:38:27 by tmaraval         ###   ########.fr       */
->>>>>>> de94e38ae82ae9eebac4cfcefaa10d044ccfb368
+/*   Updated: 2018/05/11 18:14:20 by cormarti         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "../../includes/lexer.h"
-#include "../../includes/ext_fun_type.h"
-#include <stdio.h>
+#include <unistd.h>
 
 t_tkn		*tkn_init(int len)
 {
@@ -100,7 +94,6 @@ static void	state_dquoted(t_tkn **head, char **str, t_tkn_state *state)
 	int		len;
 	t_tkn	*tkn;
 
-<<<<<<< HEAD
 	line = *str;
 	len = 0;
 	while (line[len] != '\0' && line[len] != CHR_DQUOTE)
@@ -116,7 +109,7 @@ static void	state_dquoted(t_tkn **head, char **str, t_tkn_state *state)
 	*str = line;
 }
 
-static void	lex(char **str)
+t_tkn		*lex(char **str)
 {
 	int		i;
 	t_tkn	*tkn;
@@ -126,21 +119,11 @@ static void	lex(char **str)
 	line = ft_strdup(*str);
 	tkn = tkn_init(1);
 	state = STATE_IDLE;
-	while (*line != '\0')
-=======
-	j = 0;
-	t_token *tok;
-	state = STATE_NORMAL;
+	//while (*line != '\0')
 	i = 0;
-	l_line = (int)ft_strlen(line);
-	tok = lexer_token_create(l_line);
-	*root_tok = tok;
-	if (line == 0)
-		return (0);
-	if (line == NULL)
-		return (-1);
+	if (line == 0 || line == NULL)
+		return (NULL);
 	while (line[i] != '\0')
->>>>>>> de94e38ae82ae9eebac4cfcefaa10d044ccfb368
 	{
 		i = 0;
 		if (state == STATE_IDLE)
@@ -150,12 +133,6 @@ static void	lex(char **str)
 		else if (state == STATE_QUOTED)
 			state_quoted(&tkn, &line, &state);
 	}
-	print_tkn_struct(tkn);
-}
-
-int			main(int argc, char **argv)
-{
-	(void)argc;
-	lex(&argv[1]);
-	return (0);
+	//print_tkn_struct(tkn);
+	return (tkn);
 }
