8048eaf12bfd72a54124f0d152042d8fa3409cf0
Tom MARAVAL <tmaraval@e1r11p23.42.fr>
2018-06-04 12:22:58 +0200

    utilisation de la fonction path pour exec

--- sources/exec/cmd.c
+++ sources/exec/cmd.c
@@ -6,7 +6,7 @@
 /*   By: cormarti <marvin@42.fr>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2018/06/01 21:20:02 by cormarti          #+#    #+#             */
-/*   Updated: 2018/06/02 05:10:32 by cormarti         ###   ########.fr       */
+/*   Updated: 2018/06/04 12:22:20 by tmaraval         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -37,7 +37,11 @@ char	**lst_arr(t_tkn **tkn)
 	i = 0;
 	while (tkn[i] && tkn[i]->data)
 	{
-		args[i] = strdup(tkn[i]->data);
+		if (!ft_strcmp(tkn[i]->data, "\n"))
+			args[i] = 0;
+		else
+			args[i] = strdup(tkn[i]->data);
+
 		i++;
 	}
 	return (args);
@@ -46,7 +50,10 @@ char	**lst_arr(t_tkn **tkn)
 int		exec_cmd(t_astree *astree, char **env)
 {
 	char	**args;
+	char	*cmd;
+	int		i;
 
+	i = 0;
 	if (is_redirected(astree->arg))
 	{
 		ft_putendl("is redirected, exit now");
@@ -56,7 +63,9 @@ int		exec_cmd(t_astree *astree, char **env)
 	ft_putendl("copy tkn data to str array");
 	args = lst_arr(astree->arg);
 	ft_putstr("execute cmd: ");
-	ft_putendl(args[0]);	
-	execve(args[0], args, env);
+	ft_putendl(args[0]);
+	cmd = path_find_in_path(args[0], env);
+	execve(cmd, args, env);
+	free(cmd);
 	return (1);
 }
